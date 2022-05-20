ed9d88e8e321d6400100bad441c8c2226f4fbf09
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-05-20 13:20:30 +0800

    MOD

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Loading from "./components/Loading";
 import Error from "./components/Error";
 import { useState } from "react";
 import Link from "./components/Link";
+import moment from "moment";
 
 function App() {
   const [data, loading, error] = useFetch("https://randomuser.me/api");
@@ -28,13 +29,23 @@ function App() {
   const styleInner = {
     paddingTop: "15vh",
   };
-
+  let date = data && data.results[0].dob.date;
+  let momentDate = moment(date).format("YYYY оны MM сарын DD ны өдөр төрсөн");
+  console.log(data);
   return (
     <div className="App" s>
       <Header />
       <div style={styleInner}>
-        <p>{data && data.results[0].cell}</p>
+        <h3>
+          Name: <span>{data?.results[0].name.title}</span>
+          <> </>
+          <span>{data?.results[0].name.first}</span>
+          <> </>
+          <span>{data?.results[0].name.last}</span>
+        </h3>
+        <h3>Phone: {data?.results[0].cell}</h3>
         <img src={data?.results[0].picture.large} alt="" />
+        <h3>{momentDate}</h3>
       </div>
       <div style={style.outer}>
         {names.map((e, i) => {
