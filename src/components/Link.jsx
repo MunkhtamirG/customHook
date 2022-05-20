1a3bf514ca1cd8d1c716e0429a3716c197f1520b
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-05-20 09:23:00 +0800

    MOD

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -6,17 +6,19 @@ export default function Link(props) {
       display: "flex",
       width: "100%",
       justifyContent: "space-around",
+      marginTop: "40px",
     },
     inner: {
-      backgroundColor: props.selected ? "gray" : "white",
+      backgroundColor: props.selected ? "teal" : "orange",
       padding: "20px",
       borderRadius: "20px",
       fontSize: "40px",
+      marginTop: "40px",
     },
   };
   return (
     <div>
-      <h3 style={style.inner} onClick={props.onclick}>
+      <h3 style={style.inner} onClick={props.onclick} className="btn  ">
         {props.name}
       </h3>
     </div>
